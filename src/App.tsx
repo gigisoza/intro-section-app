eacb2a32a0d407ba08f4f29ecda4aebe2e0a0fe4
gigisoza <sozashviligigi@gmail.com>
2023-05-04 00:20:10 +0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import desktopImg from "./images/image-hero-desktop.png";
+import mobileImg from "./images/image-hero-mobile.png";
+
+function App() {
+  return (
+    <>
+      <section>
+        <article>
+          <h1>Make remote work</h1>
+          <p>
+            Get your team in sync, no matter your location. Streamline
+            processes, create team rituals, and watch productivity soar.
+          </p>
+          <button>Learn more</button>
+        </article>
+      </section>
+    </>
+  );
+}
+
+export default App;
